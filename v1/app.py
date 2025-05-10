fdb02cb85393abc404674cc2f93107146327f506
DarrylMillan <darryl020694@gmail.com>
2025-05-09 20:53:29 -0500

    Agregar comentarios a los codigos

--- v1/app.py
+++ v1/app.py
@@ -1,9 +1,21 @@
+# Importar la clase flask desde el modulo flask
 from flask import Flask
+
+# Crear instancia de la aplicacion flask, esta sera la aplicacion web
 app = Flask(__name__)
 
+# Definir ruta raiz '/' y su controlador, esto significa que cuando un usuario accede al dominio principal
+# vera esta respuesta
 @app.route('/')
+
 def hello():
+
+    # Retornar respuesta de aplicacion version 1
     return "¡Hola desde la versión 1!"
 
+# Ejecutar aplicacion
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+
+    # Ejecutar la aplicacion en todas las interfaces disponibles (0.0.0.0)
+    # en el puerto 5000 (puerto por defecto para flask)
+    app.run(host = '0.0.0.0', port = 5000)
